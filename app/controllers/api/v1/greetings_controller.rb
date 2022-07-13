ff3ecfd7474d726b52e6f70fcc154e42f4b5d8ca
daudi13 <davouma13@gmail.com>
2022-07-13 20:20:40 +0300

    fix linter errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -3,7 +3,7 @@ module Api
     class GreetingsController < ApplicationController
       def index
         random_greeting = Greeting.all.sample
-        render json: {status: 'success', data: random_greeting, message: 'Random greeting retrieved'}, status: 200
+        render json: { status: 'success', data: random_greeting, message: 'Random greeting retrieved' }, status: 200
       end
     end
   end
