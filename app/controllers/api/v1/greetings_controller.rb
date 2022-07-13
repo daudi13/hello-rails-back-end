dea17927664770aac76d2899c7bd07c5c375afa7
daudi13 <davouma13@gmail.com>
2022-07-13 20:14:56 +0300

    Add json body to API

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class GreetingsController < ApplicationController
       def index
-        render json: Greeting.all.sample
+        random_greeting = Greeting.all.sample
+        render json: {status: 'success', data: random_greeting, message: 'Random greeting retrieved'}, status: 200
       end
     end
   end
